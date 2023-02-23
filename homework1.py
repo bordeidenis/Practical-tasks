29c2c4579b0eb5feb93c306a3305e5d16d04daf4
Denis Bordei <bordei.den99@gmail.com>
2023-02-23 23:22:12 +0200

    added some changes

--- homework1.py
+++ homework1.py
@@ -1,6 +1,12 @@
+# я пробовал перевести файл colors.txt в формат JSON, однако получал исключение при работе с ним
+# JSONDecodeError: Expecting property name enclosed in double quotes:
+# искал решение здесь https://bobbyhadz.com/blog/python-jsondecodeerror-expecting-property-name-enclosed-in-double-quotes
+# import json
 file_path = "./colors.txt"
 with open(file_path, "r") as file:
     colors = eval(file.read())
+#    valid_colors = file.read()
+#    colors = json.loads(valid_colors)
 
 
 def get_color_name(color_code):
@@ -9,7 +15,14 @@ def get_color_name(color_code):
             return key
     return ("Такого цвета не существует")
 
-a = int(input("Введите первое число: "))
-b= int(input("Введите второе число: "))
-c = int(input("Введите третье число: "))
-print(get_color_name([a, b, c]))
+
+try:
+    a = int(input("Введите первое число: "))
+    b = int(input("Введите второе число: "))
+    c = int(input("Введите третье число: "))
+
+except ValueError:
+    print("Вы ввели не число")
+
+else:
+    print(get_color_name([a, b, c]))
