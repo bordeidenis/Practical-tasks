7ce4787ae479621ffc33150770b2b1ad6436ca79
Denis Bordei <bordei.den99@gmail.com>
2023-03-03 11:45:40 +0200

    Solve the task 1_2 for regular expressions

---
+++ task_1_2.py
@@ -0,0 +1,41 @@
+import re
+
+
+def work_with_regex():
+    with open("logger.log", "r") as f:
+        log_content = f.read()
+
+    eid_regex = re.compile(r"D:TUpdaterController::SetUniqueParam\(429\): eid:(.*)")
+    eid_lines = eid_regex.findall(log_content)
+
+    if len(eid_lines) >= 2:
+        second_to_last_eid = eid_lines[-2]
+        last_eid = eid_lines[-1]
+    else:
+        exit()
+
+    second_to_last_eid_dict = {}
+    last_eid_dict = {}
+    # я не понял почему мне second_to_last_eid IDE подсвечивает и просит сделать глобальной переменной
+    for key_value in second_to_last_eid.split(";"):
+        key, value = key_value.split(".")
+        second_to_last_eid_dict[key] = value
+    for key_value in last_eid.split(";"):
+        key, value = key_value.split(".")
+        last_eid_dict[key] = value
+
+    differences = {}
+    for key in last_eid_dict.keys():
+        if key not in second_to_last_eid_dict or last_eid_dict[key] != second_to_last_eid_dict[key]:
+            differences[key] = (second_to_last_eid_dict.get(key), last_eid_dict.get(key))
+
+    # Вывод результатов в консоль
+    print("Предпоследние eid: ", second_to_last_eid)
+    print("Последние eid: ", last_eid)
+    print("Отличия: ", differences)
+
+
+    return differences
+
+
+print(work_with_regex())
